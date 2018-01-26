6389e6b93df443e7392720e5e32cbcfcd3e75825
Sethwins <35605530+sethwins@users.noreply.github.com>
2018-01-26 10:42:28 -0700

    This is Wallet

---
+++ CSCI_Frank_Final Project/CSCI_Frank_Final Project/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace CSCI_Frank_Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.penny = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nickel = new System.Windows.Forms.NumericUpDown();
+            this.dime = new System.Windows.Forms.NumericUpDown();
+            this.quarter = new System.Windows.Forms.NumericUpDown();
+            this.silverdollar = new System.Windows.Forms.NumericUpDown();
+            this.fivedollar = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tendollars = new System.Windows.Forms.NumericUpDown();
+            this.twentydollars = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.penny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nickel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quarter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.silverdollar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivedollar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tendollars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentydollars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(416, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Amount of Money:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // penny
+            // 
+            this.penny.Location = new System.Drawing.Point(115, 12);
+            this.penny.Name = "penny";
+            this.penny.Size = new System.Drawing.Size(70, 20);
+            this.penny.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Penny";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nickel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Quarter";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Silver Dollar";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Five Dollar";
+            // 
+            // nickel
+            // 
+            this.nickel.Location = new System.Drawing.Point(115, 51);
+            this.nickel.Name = "nickel";
+            this.nickel.Size = new System.Drawing.Size(70, 20);
+            this.nickel.TabIndex = 10;
+            // 
+            // dime
+            // 
+            this.dime.Location = new System.Drawing.Point(115, 105);
+            this.dime.Name = "dime";
+            this.dime.Size = new System.Drawing.Size(70, 20);
+            this.dime.TabIndex = 11;
+            // 
+            // quarter
+            // 
+            this.quarter.Location = new System.Drawing.Point(115, 150);
+            this.quarter.Name = "quarter";
+            this.quarter.Size = new System.Drawing.Size(70, 20);
+            this.quarter.TabIndex = 12;
+            // 
+            // silverdollar
+            // 
+            this.silverdollar.Location = new System.Drawing.Point(115, 189);
+            this.silverdollar.Name = "silverdollar";
+            this.silverdollar.Size = new System.Drawing.Size(70, 20);
+            this.silverdollar.TabIndex = 13;
+            // 
+            // fivedollar
+            // 
+            this.fivedollar.Location = new System.Drawing.Point(115, 227);
+            this.fivedollar.Name = "fivedollar";
+            this.fivedollar.Size = new System.Drawing.Size(70, 20);
+            this.fivedollar.TabIndex = 14;
+            this.fivedollar.ValueChanged += new System.EventHandler(this.numericUpDown6_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(213, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Ten Dollars";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(197, 105);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Twenty Dollars";
+            // 
+            // tendollars
+            // 
+            this.tendollars.Location = new System.Drawing.Point(280, 49);
+            this.tendollars.Name = "tendollars";
+            this.tendollars.Size = new System.Drawing.Size(71, 20);
+            this.tendollars.TabIndex = 19;
+            // 
+            // twentydollars
+            // 
+            this.twentydollars.Location = new System.Drawing.Point(280, 98);
+            this.twentydollars.Name = "twentydollars";
+            this.twentydollars.Size = new System.Drawing.Size(71, 20);
+            this.twentydollars.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 280);
+            this.Controls.Add(this.twentydollars);
+            this.Controls.Add(this.tendollars);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fivedollar);
+            this.Controls.Add(this.silverdollar);
+            this.Controls.Add(this.quarter);
+            this.Controls.Add(this.dime);
+            this.Controls.Add(this.nickel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.penny);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Wallet ";
+            ((System.ComponentModel.ISupportInitialize)(this.penny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nickel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quarter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.silverdollar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivedollar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tendollars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentydollars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown penny;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nickel;
+        private System.Windows.Forms.NumericUpDown dime;
+        private System.Windows.Forms.NumericUpDown quarter;
+        private System.Windows.Forms.NumericUpDown silverdollar;
+        private System.Windows.Forms.NumericUpDown fivedollar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown tendollars;
+        private System.Windows.Forms.NumericUpDown twentydollars;
+    }
+}
+
---
+++ CSCI_Frank_Final Project/CSCI_Frank_Final Project/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MoneyLib;
+using CSCI_Frank_Final_Project;
+
+namespace CSCI_Frank_Final_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Form1 wallet = new Form1();
+        static Penny p = new Penny();
+        static Nickel n = new Nickel();
+        static Dime d = new Dime();
+        static Quarter q = new Quarter();
+        static SilverDollar sd = new SilverDollar();
+        static FiveDollarBill d5 = new FiveDollarBill();
+        static TenDollarBill d10 = new TenDollarBill();
+        static TwentyDollarBill d20 = new TwentyDollarBill();
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tempCount = 0;
+            tempCount = (int)penny.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(n);
+            }
+            tempCount = (int)dime.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(d);
+            }
+            tempCount = (int)quarter.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(q);
+            }
+            tempCount = (int)nickel.Value;
+            for (int i = 0; i < tempCount; i++ )
+            {
+                listBox1.Items.Add(n);
+            }
+            tempCount = (int)silverdollar.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(sd);
+            }
+            tempCount = (int)fivedollar.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(d5);
+            }
+            tempCount = (int)tendollars.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(d10);
+            }
+            tempCount = (int)twentydollars.Value;
+            for (int i = 0; i < tempCount; i++)
+            {
+                listBox1.Items.Add(d20);
+            }
+
+            penny.Value = 0;
+            nickel.Value = 0;
+            dime.Value = 0;
+            quarter.Value = 0;
+            silverdollar.Value = 0;
+            fivedollar.Value = 0;
+            tendollars.Value = 0;
+            twentydollars.Value = 0;
+
+            displayTotal();
+
+        }
+
+        private void displayTotal()
+        {
+            decimal temptotal = 0m;
+            foreach (CirculatingMoney mn in listBox1.Items)
+            {
+                temptotal += mn.Value;
+            }
+            textBox1.Text = temptotal.ToString();
+            
+        }
+
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
