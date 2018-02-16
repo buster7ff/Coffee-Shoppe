c4f098c538982c291c5f42ef0f1cdf69c49c3b69
buster7ff <35574346+buster7ff@users.noreply.github.com>
2018-02-16 10:39:09 -0700

    Add files via upload
    Console example of one working product with inheritance

---
+++ consoleMoneyTest/consoleMoneyTest/Pop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoneyLib;
+
+namespace consoleMoneyTest
+{
+    public class Pop
+    {
+        public Money Price;
+        public string name;
+        public int Volume;
+
+
+        public Pop(string newTitle, int newVolume, Money newAmount)
+        {
+            name = newTitle;
+            Volume = newVolume;
+            Price = newAmount;
+        }
+
+       
+    }
+
+    public class Coke : Pop
+
+       
+    {
+       public Coke() : base("Coke", 12, new Money(new Currency("USD"), 1.50m))
+        {
+
+           
+        }
+    }
+
+    public class Sprite : Pop
+
+    {
+        public Sprite() : base("Sprite", 12, new Money(new Currency("USD"), 1.75m))
+        {
+
+        }
+    }
+}
---
+++ consoleMoneyTest/consoleMoneyTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MoneyLib;
+
+
+namespace consoleMoneyTest
+{
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Coke c;
+            Sprite s;
+            c = new Coke();
+            s = new Sprite();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("A {0} costs {1}", c.name, c.Price.Value);
+            Console.WriteLine("and a {0} costs {1}", s.name, s.Price.Value);
+            Console.WriteLine("Your total is {0}", s.Price.Value + c.Price.Value);
+            Console.ReadLine();
+
+        }
+    }
+}
